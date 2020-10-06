b220cf7a4e2f7739c0640ba4dcde988a2ca18a31
Ariel <ArielR135@outlook.com>
2020-10-06 18:14:48 -0300

    product component

---
+++ client/src/components/Product.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function Product({productId, name, description, price, availability, stock, quantity, image}) {
+	return (
+		<div>
+			<aside>
+				<img src={image} alt="product image">
+			</aside>
+			<article>
+				<h2>{name}</h2>				
+				<p>{description}</p>
+				<h3>{price}</h3>
+				<h5>{availability}</h5>
+				<h5>{stock}</h5>
+				<h5>{quantity}</h5>
+			</article>
+		</div>
+	);
+}
