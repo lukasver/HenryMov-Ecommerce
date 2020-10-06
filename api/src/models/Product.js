af5ac41fb1d3e3876d81b16f4a2e8f61ecc60c85
Lukasver <l.verdiell@gmail.com>
2020-10-06 16:16:59 -0300

    modelado product OK inicial

--- api/src/models/Product.js
+++ api/src/models/Product.js
@@ -1,12 +1,51 @@
 const { DataTypes } = require('sequelize');
+const { conn } = require('../db.js');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('product', {
     name: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING(45),
       allowNull: false,
     },
+    description: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.REAL,
+      allowNull: false,
+      validate: {
+      	isNumeric: true
+      }
+    },
+    availability: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+      	isNumeric: true
+      }
+    },
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      validate: {
+      	isNumeric: true
+      }
+    },
+    Image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+      	isUrl: true
+      }
+    },
+  } , {
+  	timestamps: false
   });
 };
