e900ff64e869b9a2ffcdb400dd94f5b7e70b9077
Lukasver <l.verdiell@gmail.com>
2020-10-06 21:37:06 -0300

    agregado GET a route /products T21

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,12 +1,15 @@
 const server = require('express').Router();
 const { Product } = require('../db.js');
 
-server.get('/', (req, res, next) => {
+server.get('/products', (req, res, next) => {
 	Product.findAll()
 		.then(products => {
-			res.send(products);
+			res.json(products);
 		})
-		.catch(next);
+		.catch(err => {
+			console.log(err);
+			next()
+		});
 });
 
 //------- Searchs any product by name or description--------// 
