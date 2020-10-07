65f569e8c6320fab38608819810d479261efcade
Diego Emanuel Tolaba <diegotolaba09@gmail.com>
2020-10-07 17:12:55 -0300

    Estilo de la Pagina

---
+++ client/src/components/Slider.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import slider1 from '../img/slider1.jpg';
+import slider2 from '../img/slider2.jpg';
+import slider3 from '../img/slider3.jpg';
+import './Slider.css';
+
+export default function Slider() {
+
+    return (
+        <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div className="carousel-inner">
+                <div className="carousel-item active">
+                    <img className="d-block w-100" src={slider1} alt="First slide" />
+                    <div class="carousel-caption d-none d-md-block">
+                        <h1 className="tituloSlider">Plantillas Footprint</h1>
+                        <buttom className="btn btn-dark">Ver más</buttom>
+                    </div>
+                </div>
+                <div className="carousel-item">
+                    <img className="d-block w-100" src={slider2} alt="Second slide" />
+                </div>
+                <div className="carousel-item">
+                    <img className="d-block w-100" src={slider3} alt="Third slide" />
+                </div>
+            </div>
+            <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="sr-only">Previous</span>
+            </a>
+            <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="sr-only">Next</span>
+            </a>
+        </div>
+    );
+}
