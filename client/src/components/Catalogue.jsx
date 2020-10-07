1580c263dd142e1a330ae9d78ffbf553f641764f
cristianluca19 <cristianlucatti@gmail.com>
2020-10-07 17:32:49 -0300

    Arreglos en componetes catalogo y producto

--- client/src/components/Catalogue.jsx
+++ client/src/components/Catalogue.jsx
@@ -1,24 +1,41 @@
 import React from 'react';
 import ProductCard from './ProductCard.jsx'
 
-export default function Catalogue({catalogue}) {
-	if (catalogue) {
-		return (
-			<div>
-	//=======Ver si la tarjeta del catalogo necesita tanta info============
-				{catalogue.map(pc => <ProductCard
-					key={pc.producId}
-					name={pc.name}
-					description={pc.description}
-					price={pc.price}
-					availability={pc.availability}
-					stock={pc.stock}
-					quantity={pc.quantity}
-					image={pc.image}					
-				/> )}
-			</div>
-		);
-	} else {
-		<h3>Without products.</h3>
-	}
+
+export default function Catalogue(props) {
+	//=========================Datos de Prueba ==========================
+	const productos = [{
+		name: 'Longboard Sector 9 Lacey C/detalles Exhibicion',
+		price: '$2000',
+		image: 'https://d26lpennugtm8s.cloudfront.net/stores/001/049/128/products/2231-3432720f2779390e6615887894926411-640-0.jpg',
+		description: 'Tablas con detalles ESTÉTICOS con hasta 50% de descuento. '
+	}, {
+		name: 'Longboard Sector 9 Lacey C/detalles Exhibicion',
+		price: '$2000',
+		image: 'https://d26lpennugtm8s.cloudfront.net/stores/001/049/128/products/2231-3432720f2779390e6615887894926411-640-0.jpg',
+		description: 'Tablas con detalles ESTÉTICOS con hasta 50% de descuento. '
+	}, {
+		name: 'Longboard Sector 9 Lacey C/detalles Exhibicion',
+		price: '$2000',
+		image: 'https://d26lpennugtm8s.cloudfront.net/stores/001/049/128/products/2231-3432720f2779390e6615887894926411-640-0.jpg',
+		description: 'Tablas con detalles ESTÉTICOS con hasta 50% de descuento. '
+	}, {
+		name: 'Longboard Sector 9 Lacey C/detalles Exhibicion',
+		price: '$2000',
+		image: 'https://d26lpennugtm8s.cloudfront.net/stores/001/049/128/products/2231-3432720f2779390e6615887894926411-640-0.jpg',
+		description: 'Tablas con detalles ESTÉTICOS con hasta 50% de descuento. '
+	}]
+	//==================================================================================
+
+	return (
+		<div>
+		//=======Ver si la tarjeta del catalogo necesita tanta info============
+			{productos.map(prod => <ProductCard
+				name={prod.name}
+				description={prod.description}
+				price={prod.price}
+				image={prod.image}
+			/>)}
+		</div>
+	);
 }
--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -2,18 +2,34 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 //============= ( RECORDAR AGREGAR PARAMETRO PUNTUACION )============
-export function ProductCard({ name, price, image}){
+export default function ProductCard({ name, image, price, description }) {
+
     return (
         <div>
-            <Link to={`/product/${id}`}>
-                <div>
-                <h3>{name}</h3>
+            <Link to={`/`}>
+                <div div className="row row-cols-1 row-cols-md-2">
+                    <div className="col mb-4">
+                        <div className="card">
+                            <img src={image} className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">{name}</h5>
+                                <p className="card-text">{description}</p>
+                                <p className="card-text">{price}</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div>
-                <img>{image}</img>
-                <h4>{price}</h4>
-                </div>
+                {/* <div className="media">
+                    <div className="media-body">
+                        <h5 className="mt-0 mb-1">{name}</h5>
+                        <p className="card-text">{description}</p>
+                        <p className="card-text">{price}</p>
+                    </div>
+                    <img src={image} className="ml-3" alt="..." />
+                </div> */}
+
             </Link>
+
         </div>
     )
 }
