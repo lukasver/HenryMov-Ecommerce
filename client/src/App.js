66474eeb0bb8cf6f6e71b22980bf91fedc068247
cristianluca19 <cristianlucatti@gmail.com>
2020-10-08 20:00:48 -0300

    nada...solo acomodar el index porque cambio de tarea

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { Route } from "react-router-dom";
 import Nav from './components/Nav.jsx';
 import Product from './components/Product.jsx';
+import ProductCard from './components/ProductCard.jsx';
 import Slider from './components/Slider';
 import Catalogue from './components/Catalogue';
+import axios from 'axios';
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  function onSearch(search) {
+    axios.get(`http://localhost:3001/search?product=${search}`)
+      .then(recurso => {
+        setProducts(recurso.data);
+        return recurso.data;
+      });
+  }
+
   return (
-    <div className="App">
-      <Route path="/" render={Nav}/>
-      <Route exact path="/" render={Slider}/>
-      <Route exact path="/products" render={Catalogue}/>
-      <Route exact path="/products/:id" render={Product}/>
+
+   <div className="App">
+     <Route path="/" render={() => <Nav onSearch={onSearch} />} />
+	  <Route exact path="/search" render={() => <ProductCard products={products} />} />
+		<Route exact path="/" render={() => <Slider/>}/>
+		<Route exact path="/products" render={() => <Catalogue/>}/>
+		<Route exact path="/products/:id" render={() => <Product/>}/>
+
     </div>
   );
 }
--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -3,21 +3,28 @@ import { Link } from 'react-router-dom';
 import './ProductCard.css';
 
 //============= ( RECORDAR AGREGAR PARAMETRO PUNTUACION )============
-export default function ProductCard({ name, image, price, description, id }) {
-
-    return (
-        <div className="card-group">
-            <Link to={`/products/${id}`}>
-              <div className="card">
-                <img className="card-img-top" src={image} alt="imagen producto"/>
-                <div className="card-body">
-                  <h5 className="card-title">{name}</h5>
-                  <p className="card-text">{description}</p>
-                  <p className="card-text">{price}</p>
-                  <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+export default function ProductCard(props) {
+  return (
+    <div className="container">
+      <div className="main row">
+        {
+          props.products.map(producto => {
+            return (
+              <div className="card-group  col-md-3">
+                <div className="card">
+                  <img className="card-img-top" src={producto.image} alt="imagen producto" />
+                  <div className="card-body">
+                    <h5 className="card-title">{producto.name}</h5>
+                    <p className="card-text">{producto.description}</p>
+                    <p className="card-text">{`$ ${producto.price}`}</p>
+                  </div>
                 </div>
-             </div>
-            </Link>
-        </div>
-    )
+              </div>
+            )
+          })
+        }
+      </div>
+    </div>
+
+  )
 }
