8b558d71f3ae83c0992d6ea53afd341d4db77926
Diego Emanuel Tolaba <diegotolaba09@gmail.com>
2020-10-08 18:24:40 -0300

    Cambios en NavBar Search

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,5 +1,7 @@
 const server = require('express').Router();
 const { Product } = require('../db.js');
+const { Sequelize } = require('sequelize');
+const Op = Sequelize.Op;
 
 server.get('/products', (req, res, next) => {
 	Product.findAll()
@@ -9,25 +11,25 @@ server.get('/products', (req, res, next) => {
 		.catch(err => {
 			console.log(err);
 			next()
-		});	
+		});
 });
 
-server.get('/product/:id', (req, res, next)=>{
-//=============================================
-//  Obtener por producto por id (unico) (fijarse si funciona sin 'id:id')
-//=============================================
+server.get('/product/:id', (req, res, next) => {
+	//=============================================
+	//  Obtener por producto por id (unico) (fijarse si funciona sin 'id:id')
+	//=============================================
 	const { id } = req.params;
 	Product.findOne({
-		where:{
+		where: {
 			id
 		}
 	})
-	.then(product => {
-		res.status(200).json(product);
-	})
-	.catch(error =>{
-		res.status(404).send('<h1>error...product not found</h1>')
-	})
+		.then(product => {
+			res.status(200).json(product);
+		})
+		.catch(error => {
+			res.status(404).send('<h1>error...product not found</h1>')
+		})
 })
 
 //==============================================
@@ -37,19 +39,20 @@ server.get('/search', (req, res, next) => {
 	const { product } = req.query;
 	Product.findAll({
 		where: {
-			$or: [
+			[Op.or]: [
 			{
 				name: { 
-					$iLike: '%' + product + '%'}
+					[Op.iLike]: '%' + product + '%'}
 			},
 			{
 				description: {
-					$iLike: '%' + product + '%'
+					[Op.iLike]: '%' + product + '%'
 				}
 			}]
 		}
 	})
 		.then(product => {
+			console.log('producto')
 			res.json(product)
 		})
 		.catch(error => {
