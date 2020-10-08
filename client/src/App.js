0baff1a642589613fd9375170508029919c85a3c
lukasver <l.verdiell@gmail.com>
2020-10-08 09:22:53 -0300

    routing app agregado catalogo y modificado productcard para que se muestre en catalogo

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,15 @@ import { Route } from "react-router-dom";
 import Nav from './components/Nav.jsx';
 import Product from './components/Product.jsx';
 import Slider from './components/Slider';
+import Catalogue from './components/Catalogue';
 
 function App() {
   return (
     <div className="App">
-      {/* <Route
-        path="/"
-        render={() => <Nav />}
-      />
-      <Slider />
-      <Route
-        exact path="products/:id"
-        render={<Product />}
-      /> */}
-      <Product />
+      <Route path="/" render={Nav}/>
+      <Route exact path="/" render={Slider}>
+      <Route exact path="/products" render={Catalogue}/>
+      <Route exact path="/products/:id" render={Product}/>
     </div>
   );
 }
--- client/src/components/Catalogue.jsx
+++ client/src/components/Catalogue.jsx
@@ -1,41 +1,91 @@
 import React from 'react';
-import ProductCard from './ProductCard.jsx'
+import ProductCard from './ProductCard.jsx';
+import './Catalogue.css';
 
 
 export default function Catalogue(props) {
 	//=========================Datos de Prueba ==========================
 	const productos = [{
+		id: 1,
 		name: 'Longboard Sector 9 Lacey C/detalles Exhibicion',
 		price: '$2000',
 		image: 'https://d26lpennugtm8s.cloudfront.net/stores/001/049/128/products/2231-3432720f2779390e6615887894926411-640-0.jpg',
 		description: 'Tablas con detalles ESTÉTICOS con hasta 50% de descuento. '
 	}, {
+		id: 2,
 		name: 'Longboard Sector 9 Lacey C/detalles Exhibicion',
 		price: '$2000',
 		image: 'https://d26lpennugtm8s.cloudfront.net/stores/001/049/128/products/2231-3432720f2779390e6615887894926411-640-0.jpg',
 		description: 'Tablas con detalles ESTÉTICOS con hasta 50% de descuento. '
 	}, {
+		id: 3,
 		name: 'Longboard Sector 9 Lacey C/detalles Exhibicion',
 		price: '$2000',
 		image: 'https://d26lpennugtm8s.cloudfront.net/stores/001/049/128/products/2231-3432720f2779390e6615887894926411-640-0.jpg',
 		description: 'Tablas con detalles ESTÉTICOS con hasta 50% de descuento. '
 	}, {
+		id: 4,
+		name: 'Longboard Sector 9 Lacey C/detalles Exhibicion',
+		price: '$2000',
+		image: 'https://d26lpennugtm8s.cloudfront.net/stores/001/049/128/products/2231-3432720f2779390e6615887894926411-640-0.jpg',
+		description: 'Tablas con detalles ESTÉTICOS con hasta 50% de descuento. '
+	}, {
+		id: 5,
+		name: 'Longboard Sector 9 Lacey C/detalles Exhibicion',
+		price: '$2000',
+		image: 'https://d26lpennugtm8s.cloudfront.net/stores/001/049/128/products/2231-3432720f2779390e6615887894926411-640-0.jpg',
+		description: 'Tablas con detalles ESTÉTICOS con hasta 50% de descuento. '
+	}, {
+		id: 6,
 		name: 'Longboard Sector 9 Lacey C/detalles Exhibicion',
 		price: '$2000',
 		image: 'https://d26lpennugtm8s.cloudfront.net/stores/001/049/128/products/2231-3432720f2779390e6615887894926411-640-0.jpg',
 		description: 'Tablas con detalles ESTÉTICOS con hasta 50% de descuento. '
 	}]
+
+	const categories = ['Scooters','Skates','Windsurf','Bikes'];
 	//==================================================================================
 
 	return (
-		<div>
-		//=======Ver si la tarjeta del catalogo necesita tanta info============
-			{productos.map(prod => <ProductCard
-				name={prod.name}
-				description={prod.description}
-				price={prod.price}
-				image={prod.image}
-			/>)}
-		</div>
+				<div className="grid">
+					<div className="categorycol">
+						<h2>Categorias</h2>
+						{categories.map(category => <div>{category}  <input type='checkbox'/></div>)}
+					</div>
+					<div className="gridcards">
+					{productos.map(prod => 
+						<div>
+							<ProductCard
+							key={prod.id}
+							id={prod.id}
+							name={prod.name}
+							description={prod.description}
+							price={prod.price}
+							image={prod.image}
+							/>
+						</div>)}
+					</div>
+				</div>
+
 	);
 }
+
+				// <div className="container-fluid">
+				// 	<div className="main row">
+				// 		<div className="col-md-3 categorycol">
+				// 		<h4>Categorias</h4>
+				// 		{categories.map(category => <div><input type='checkbox'/>{category}</div>)}
+				// 		</div>
+				// 		<div className="col-md-9 row">
+				// 			{productos.map(prod => 
+				// 				<div className="col-sm-3">
+				// 					<ProductCard
+				// 					name={prod.name}
+				// 					description={prod.description}
+				// 					price={prod.price}
+				// 					image={prod.image}
+				// 					/>
+				// 				</div>)}
+				// 			</div>
+				// 		</div>
+				// </div>
