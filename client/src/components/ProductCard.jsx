045bd8518dad8f2085160f06934b232378db183b
unknown <sebalevin@hotmail.com.ar>
2020-10-09 01:14:12 -0300

    Link

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,30 +1,29 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './ProductCard.css';
+  import React from 'react';
+  import { Link } from 'react-router-dom';
+  import './ProductCard.css';
 
-//============= ( RECORDAR AGREGAR PARAMETRO PUNTUACION )============
-export default function ProductCard(props) {
-  return (
-    <div className="container">
-      <div className="main row">
-        {
-          props.products.map(producto => {
-            return (
-              <div className="card-group  col-md-3">
-                <div className="card">
-                  <img className="card-img-top" src={producto.image} alt="imagen producto" />
-                  <div className="card-body">
-                    <h5 className="card-title">{producto.name}</h5>
-                    <p className="card-text">{producto.description}</p>
-                    <p className="card-text">{`$ ${producto.price}`}</p>
+  //============= ( RECORDAR AGREGAR PARAMETRO PUNTUACION )============
+  export default function ProductCard(props) {
+    const { name, image, price, description, id } = props;
+
+    return (
+      <div className="container">
+        <div className="main row">
+                <div className="card-group  col-md-9">
+                  <div className="card">
+                    <img className="card-img-top" src={image} alt="imagen producto" />
+                    <div className="card-body">
+                      <Link to='/products/name'>
+                        <h5 className="card-title">{name}</h5>
+                      </Link>
+                      <p className="card-text">{description}</p>
+                      <p className="card-text">{`$ ${price}`}</p>
+                    </div>
                   </div>
                 </div>
-              </div>
-            )
-          })
-        }
+        </div>
+        algoooo
       </div>
-    </div>
 
-  )
-}
+    )
+  }
