bebfe4e6d2d29ba44befe79d046ab7540332c15a
cristianluca19 <cristianlucatti@gmail.com>
2020-10-10 01:29:47 -0300

    formulario producto funcionando...dia largo pero productivo, a desacansar

--- client/src/components/AddProduct.jsx
+++ client/src/components/AddProduct.jsx
@@ -2,172 +2,106 @@ import React, { useState, handleChange, handleSubmit, useEffect } from 'react';
 import './AddProduct.css'
 import axios from 'axios';
 
-const urlBack='http://localhost:3001'
+const urlBack = 'http://localhost:3001'
 
 export default function AddProduct() {
-    const [titulo, setTitulo] = useState('')
-    const [descripcion, setDescripcion] = useState('')
-    const [cantidad, setCantidad] = useState(0)
-    const [disponible, setDisponible] = useState(true)
-    const [categorias, setCategorias] = useState('')
-    const [precio, setPrecio] = useState(0)
-    const [imagen, setImagen] = useState('')
 
-    const fileInput = React.createRef();
-
-
+    const [addProd, setAddProd] = useState({
+        name: '',
+        description: '',
+        price: '0',
+        availability: 'false',
+        stock: ' 0',
+        image: '',
+        categories: ''
+    })
 
     function handleChange(e) {
-        setTitulo({ value: e.target.value });
-        setDescripcion({ value: e.value })
-        setPrecio({ value: e.value })
-        console.log(titulo)
+        setAddProd({
+            ...addProd,
+            [e.target.id]: e.target.value
+        })
     }
-    
 
-     async function handleSubmit(e) {
+    const handleAdd = e => {
         e.preventDefault();
-       
-      
-     }
+        console.log(addProd)
+        try {
+            // Creates the new product
+            axios.post(`${urlBack}/products`, addProd);
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
-
-        const handleAdd = async event => {
-            event.preventDefault();
-            
-            let newProducto = {
-                name:  'titulo.value',
-                description:  'descripcion.value',
-                price: '2323232' ,
-                availability: true,
-                stock:  '0' ,
-                image: ' imagen.value',
-                categories: 'categorias.value',
-              
-            }
-            console.log(newProducto)
-           
-    
-             try {
-                // Creates the new product
-                await axios.post(`${urlBack}/products`, newProducto);
-            } catch (error) {
-               console.log(error);
-            }
-        };
-     
-    
-    
     function handleInputChange(e) {
-                    const target = e.target;
-                    const value = target.type === 'checkbox' ? target.checked : target.value;
-                    const name = target.name;
-                    setDisponible({ value })
-                }
-    
+        e.preventDefault()
+        const target = e.target.value;
+        const value = target.type === 'checkbox' ? 'true' : 'false';
+        setAddProd({
+            ...addProd, availability: value
+        })
+    }
+
+    function Categoria(e) {
+        e.preventDefault()
+        console.log(addProd)
+        console.log(e.target.value)
+        setAddProd({ ...addProd, categories: e.target.value })
+    }
+
+    function UpImagen(e) {
+        e.preventDefault()
+        const encodedData = window.btoa([e.target.value]); // encode a string
+        setAddProd({
+            ...addProd, image: encodedData
+        })
+    }
+
     return (
 
-            <div className='contenidoForm'>
-
-                <h1>Agregar Producto</h1>
-                <form class='formulario' onSubmit={handleSubmit} >
-                    <label>
-                        Titulo del producto
-        <input type="text" value={titulo.value} onChange={handleChange} />
-                    </label>
-                    <label>
-                        Descripcion
-          <textarea value={descripcion.value} onChange={handleChange} />
-                    </label>
-                    <br />
-                    <label>Categoria</label>
-                    <select>
-                        <option default value='Skates' >Skates</option>
-                        <option value='Scooters'>Scooters</option>
-                        <option value='Windsurf'>Windsurf</option>
-                        <option value='Bikes'>Bikes</option>
-                        <option value='Electric'>Electric</option>
-                    </select>
-                    <label>
-                        Disponible:
-          <input
-                            name="disponible"
-                            type="checkbox"
-                            checked={disponible.value}
-                            onChange={handleInputChange} />
-                    </label>
-                    <br />
-                    <label>
-                        Cantidad:
-          <input
-                            name="cantidad"
-                            type="number"
-                            />
-                    </label>
-                    <label>
-                        Precio:
-          <input
-                            name="number"
-                            value={precio.value}
-                            onChange={handleChange} />
-                    </label>
-                    <input type="file" ref={fileInput} />
-                    <button onClick={handleAdd}>Agregar</button>
-                </form>
-
-            </div>
+        <div className='contenidoForm'>
+            <h1>Agregar Producto</h1>
+            <form class='formulario' onSubmit={handleSubmit}  >
+                <label> Titulo del producto
+                    <input type="text" id='name' onChange={handleChange} />
+                </label>
+                <label> Descripcion
+                    <textarea id='description' onChange={handleChange} />
+                </label>
+                <br />
+                <label>Categoria</label>
+                <select onChange={Categoria} >
+                    <option default id='Skates' value='Skates' onChange={Categoria} >Skates</option>
+                    <option id='Scooters' onChange={Categoria}>Scooters</option>
+                    <option id='Windsurf' onChange={Categoria}>Windsurf</option>
+                    <option id='Bikes' onChange={Categoria}>Bikes</option>
+                    <option id='Electric' onChange={Categoria}>Electric</option>
+                </select>
+                <label> Disponible:
+                     <input
+                        id="availability"
+                        type="checkbox"
+                        checked={addProd.availability}
+                        onChange={handleInputChange} />
+                </label>
+                <br />
+                <label> Cantidad:
+                    <input
+                        id="stock"
+                        type="number"
+                        onChange={handleChange} />
+                </label>
+                <label> Precio:
+                    <input
+                        id="price"
+                        onChange={handleChange} />
+                </label>
+                <input type="file" onChange={UpImagen} />
+                <button onClick={handleAdd}>Agregar</button>
+            </form>
+        </div>
+    )
+}
 
 
-            //         <form className='contenidoForm'>
-            //       <h1>Agregar Producto</h1>
-            //   <div class="form-group">
-            //     <label for="inputAddress">Titulo</label>
-            //     <input type="text" class="form-control" id="inputAddress" placeholder="Titulo con el que se va a presentar el producto"/>
-            //   </div>
-            //   <div class="form-row">
-            //     <div class="form-group col-md-6">
-            //       <label for="inputEmail4">Email</label>
-            //       <input type="email" class="form-control" id="inputEmail4"/>
-            //     </div>
-            //     <div class="form-group col-md-6">
-            //       <label for="inputPassword4">Password</label>
-            //       <input type="password" class="form-control" id="inputPassword4"/>
-            //     </div>
-            //   </div>
-            //   <div class="form-group">
-            //     <label for="inputAddress2">Address 2</label>
-            //     <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor"/>
-            //   </div>
-            //   <div class="form-row">
-            //     <div class="form-group col-md-6">
-            //       <label for="inputCity">City</label>
-            //       <input type="text" class="form-control" id="inputCity"/>
-            //     </div>
-            //     <div class="form-group col-md-4">
-            //       <label for="inputState">State</label>
-            //       <select id="inputState" class="form-control">
-            //         <option selected>Choose...</option>
-            //         <option>...</option>
-            //       </select>
-            //     </div>
-            //     <div class="form-group col-md-2">
-            //       <label for="inputZip">Zip</label>
-            //       <input type="text" class="form-control" id="inputZip"/>
-            //     </div>
-            //   </div>
-            //   <div class="form-group">
-            //     <div class="form-check">
-            //       <input class="form-check-input" type="checkbox" id="gridCheck"/>
-            //       <label class="form-check-label" for="gridCheck">
-            //         Check me out
-            //       </label>
-            //     </div>
-            //   </div>
-            //   <button type="submit" class="btn btn-primary">Sign in</button>
-            // </form>
-
-        )
-    }
-
-
-//<input type="submit" value="Submit" />
