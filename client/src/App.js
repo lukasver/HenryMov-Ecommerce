8109fac2eb05433937cd630f7c7258737298d027
Diego Emanuel Tolaba <diegotolaba09@gmail.com>
2020-10-10 23:32:13 -0300

    App js sincronizado con el actual y cambios en el index html

--- client/src/App.js
+++ client/src/App.js
@@ -1,67 +1,78 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 import { Route, useHistory } from "react-router-dom";
-import Nav from './components/Nav.jsx';
-import Product from './components/Product.jsx';
-import ProductCard from './components/ProductCard.jsx';
-import Slider from './components/Slider';
-import Catalogue from './components/Catalogue';
-import ContentSearch from './components/ContentSearch';
-import axios from 'axios';
-import AddProduct from './components/AddProduct';
+import Nav from "./components/Nav.jsx";
+import Product from "./components/Product.jsx";
+import ProductCard from "./components/ProductCard.jsx";
+import Slider from "./components/Slider";
+import Catalogue from "./components/Catalogue";
+import ContentSearch from "./components/ContentSearch";
+import axios from "axios";
+import AddProduct from "./components/AddProduct";
+import AddCategory from "./components/AddCategory";
 
 
-function App() {
+function App({ location }) {
   const [products, setProducts] = useState([]);
-  const [totalProds,setTotalprods] = useState([]);
+  const [categories, setCategories]= useState([])
+  const [totalProds, setTotalprods] = useState([]);
 
   let history = useHistory();
 
   function onSearch(search) {
-    axios.get(`http://localhost:3001/search?product=${search}`)
-      .then(recurso => {
+    axios
+      .get(`http://localhost:3001/search?product=${search}`)
+      .then((recurso) => {
         setProducts(recurso.data);
         history.push("/search");
-        console.log(recurso.data)
+        console.log(recurso.data);
         return recurso.data;
       });
   }
 
-    useEffect(async () => {
-    await axios.get('http://localhost:3001/products')
-    .then(productosDB => {
-      const {data} = productosDB
-      return data
-    })
-    .then(listadoProductos => {
-      setTotalprods(listadoProductos
-
-        )
-    })
-    .catch(err => new Error(err))
-  },[]) 
-
+  useEffect(async () => {
+    await axios
+      .get("http://localhost:3001/products")
+      .then((productosDB) => {
+        const { data } = productosDB;
+        return data;
+      })
+      .then((listadoProductos) => {
+        setTotalprods(listadoProductos);
+      })
+      .catch((err) => new Error(err));
+    await axios
+      .get("http://localhost:3001/category")
+      .then((recurso) => {
+        console.log(recurso.data)
+        setCategories(recurso.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   function onFilter(productId) {
-    let filtro = totalProds.filter(c => c.id === parseInt(productId));
-    if(filtro.length > 0) {
-        return filtro[0];
+    let filtro = totalProds.filter((c) => c.id === parseInt(productId));
+    if (filtro.length > 0) {
+      return filtro[0];
     } else {
-        return null;
+      return null;
     }
   }
 
-
-
-
   return (
     <div className="App">
+      <Switch>
+        <Route path="/admin" render={() => <Admin />} />
         <Route path="/" render={() => <Nav onSearch={onSearch} />} />
-        <Route exact path="/search" render={() => <ContentSearch products={products} />} />
-        <Route exact path="/" render={() => <Slider/>}/>
-        <Route exact path="/products" render={() => <Catalogue listado={totalProds}/>}/>
-        <Route exact path="/products/:productId" render={({match}) => <Product product={onFilter(match.params.productId)}/>}/>
-        <Route exact path= "/product/add" render={()=><AddProduct/>}/>
+      </Switch>
+      <Route exact path="/search" render={() => <ContentSearch products={products} />} />
+      <Route exact path="/" render={() => <Slider />} />
+      <Route exact path="/products" render={() => <Catalogue listado={totalProds} />} />
+      <Route exact path="/products/:productId" render={({ match }) => <Product product={onFilter(match.params.productId)} />} />
+      <Route exact path="/product/add" render={() => <AddProduct />} />
+      <Route exact path='/category/add' render={() => <AddCategory />} />
     </div>
   );
 }
