a6b128e32fb42a3936a7d6e7f048304c31a5ea27
cristianluca19 <cristianlucatti@gmail.com>
2020-10-11 16:04:12 -0300

    funcion put completa para agregar a admin

--- api/src/app.js
+++ api/src/app.js
@@ -3,13 +3,13 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
-
+const  cors = require('cors')
 require('./db.js');
 
 const server = express();
 
 server.name = 'API';
-
+server.use(cors())
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
