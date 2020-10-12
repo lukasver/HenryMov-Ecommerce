418be88f31e4daaf4255587af9529c74f9cf3aa4
Diego Emanuel Tolaba <diegotolaba09@gmail.com>
2020-10-12 17:08:52 -0300

    Arreglos en el Panel de Administracion

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -22,7 +22,7 @@ function Nav({ onSearch }) {
             <div className="col-md-3 utilidades">
                 <div className="main row">
                     <div className="col-md-4">
-                        <Link to='/admin'>
+                        <Link to='/chat'>
                             <a className="linkIcons">
                                 <svg width="1.8em" height="1.8em" viewBox="0 0 16 16" class="bi bi-chat-dots" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                     <path fill-rule="evenodd" d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z" />
@@ -34,7 +34,7 @@ function Nav({ onSearch }) {
                         </Link>
                     </div>
                     <div className="col-md-4">
-                        <Link to='/product/add'>
+                        <Link to='/admin'>
                             <a className="linkIcons">
                                 <svg width="1.8em" height="1.8em" viewBox="0 0 16 16" class="bi bi-person" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                     <path fill-rule="evenodd" d="M10 5a2 2 0 1 1-4 0 2 2 0 0 1 4 0zM8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm6 5c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
