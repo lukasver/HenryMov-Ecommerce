1c282d3428045a671e876471c04e84c9e2938ade
Diego Emanuel Tolaba <diegotolaba09@gmail.com>
2020-10-12 18:46:07 -0300

    Productos Admin

--- client/src/components/admin/Productos.jsx
+++ client/src/components/admin/Productos.jsx
@@ -95,8 +95,6 @@ export default function Productos({ productos, categories, deleteProduct, getPro
         } else {
             setModProd({ ...modProd, availability: checked });
         }
-
-
     }
 
     return (
