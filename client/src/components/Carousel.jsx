44a1793d369f3102dde279f1da6ba3c024592f58
Diego Emanuel Tolaba <diegotolaba09@gmail.com>
2020-10-12 23:34:43 -0300

    Cambios Landing Page

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -1,171 +1,88 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './Carousel.css';
+import ProductCard from './ProductCard';
 
-export default function Carousel() {
+export default function Carousel({randomProduct, prodDes}) {
 
+    const [randomDes, setRandomDes] = useState([]);
+    
+    useEffect(() => {
+        if(prodDes.length === 0) {
+            randomProduct();            
+        } else {
+            setRandomDes(prodDes);
+        }
+    },[prodDes])
+    
     return (
-        <div id="multi-item-example" calssName="carousel slide carousel-multi-item" data-ride="carousel">
-
-            <div calssName="controls-top">
-                <a calssName="btn-floating" href="#multi-item-example" data-slide="prev"><i calssName="fa fa-chevron-left"></i></a>
-                <a calssName="btn-floating" href="#multi-item-example" data-slide="next"><i calssName="fa fa-chevron-right"></i></a>
+        <div className="container">
+            <br /><br /><br /><br /><br />
+            <h3>Productos Destacados</h3>
+            <br />
+            <div className="main row">                
+                {randomDes.map(prod =>
+                    <div className="card-group col-md-3">
+                        <ProductCard
+                            key={prod.id}
+                            id={prod.id}
+                            name={prod.name}
+                            description={prod.description}
+                            price={prod.price}
+                            image={prod.image}
+                        />
+                    </div>)}
             </div>
-
-
-
-            <ol calssName="carousel-indicators">
-                <li data-target="#multi-item-example" data-slide-to="0" calssName="active"></li>
-                <li data-target="#multi-item-example" data-slide-to="1"></li>
-                <li data-target="#multi-item-example" data-slide-to="2"></li>
-            </ol>
-
-
-
-            <div calssName="carousel-inner" role="listbox">
-
-
-                <div calssName="carousel-item active">
-
-                    <div calssName="row">
-                        <div calssName="col-md-4">
-                            <div calssName="card mb-2">
-                                <img calssName="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg"
-                                    alt="Card image cap" />
-                                <div calssName="card-body">
-                                    <h4 calssName="card-title">Card title</h4>
-                                    <p calssName="card-text">Some quick example text to build on the card title and make up the bulk of the
-                card's content.</p>
-                                    <a calssName="btn btn-primary">Button</a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div calssName="col-md-4 clearfix d-none d-md-block">
-                            <div calssName="card mb-2">
-                                <img calssName="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(18).jpg"
-                                    alt="Card image cap" />
-                                <div calssName="card-body">
-                                    <h4 calssName="card-title">Card title</h4>
-                                    <p calssName="card-text">Some quick example text to build on the card title and make up the bulk of the
-                card's content.</p>
-                                    <a calssName="btn btn-primary">Button</a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div calssName="col-md-4 clearfix d-none d-md-block">
-                            <div calssName="card mb-2">
-                                <img calssName="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(35).jpg"
-                                    alt="Card image cap" />
-                                <div calssName="card-body">
-                                    <h4 calssName="card-title">Card title</h4>
-                                    <p calssName="card-text">Some quick example text to build on the card title and make up the bulk of the
-                card's content.</p>
-                                    <a calssName="btn btn-primary">Button</a>
-                                </div>
-                            </div>
-                        </div>
+            <br /><br /><br /><br /><br /><br />
+            <h3>Testimonios</h3>
+            <br />
+            <div className="main row">
+                <div className="col-md-6 row test-izq">
+                    <div className="col-md-6">
+                        <img className="img-testimonio" src="https://www.jovenesprogramadores.cl/wp/wp-content/uploads/2020/07/foto-tertimonio-1.png" />
                     </div>
-
-                </div>
-
-
-
-                <div calssName="carousel-item">
-
-                    <div calssName="row">
-                        <div calssName="col-md-4">
-                            <div calssName="card mb-2">
-                                <img calssName="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(60).jpg"
-                                    alt="Card image cap" />
-                                <div calssName="card-body">
-                                    <h4 calssName="card-title">Card title</h4>
-                                    <p calssName="card-text">Some quick example text to build on the card title and make up the bulk of the
-                card's content.</p>
-                                    <a calssName="btn btn-primary">Button</a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div calssName="col-md-4 clearfix d-none d-md-block">
-                            <div calssName="card mb-2">
-                                <img calssName="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(47).jpg"
-                                    alt="Card image cap" />
-                                <div calssName="card-body">
-                                    <h4 calssName="card-title">Card title</h4>
-                                    <p calssName="card-text">Some quick example text to build on the card title and make up the bulk of the
-                card's content.</p>
-                                    <a calssName="btn btn-primary">Button</a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div calssName="col-md-4 clearfix d-none d-md-block">
-                            <div calssName="card mb-2">
-                                <img calssName="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(48).jpg"
-                                    alt="Card image cap" />
-                                <div calssName="card-body">
-                                    <h4 calssName="card-title">Card title</h4>
-                                    <p calssName="card-text">Some quick example text to build on the card title and make up the bulk of the
-                card's content.</p>
-                                    <a calssName="btn btn-primary">Button</a>
-                                </div>
-                            </div>
-                        </div>
+                    <div className="col-md-6">
+                        <br />
+                        <h5>Carla Basaul</h5>
+                        <p className="perfil">Programmer</p>
+                        <p className="test-text">
+                            “Supe de ustedes por un amigo. Quiero actualizar y adquirir conocimientos en lenguajes que no conocía o que conocía pero por muchos años no practiqué y ya no recuerdo.
+					</p>
                     </div>
-
                 </div>
-
-
-
-                <div calssName="carousel-item">
-
-                    <div calssName="row">
-                        <div calssName="col-md-4">
-                            <div calssName="card mb-2">
-                                <img calssName="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/Food/4-col/img%20(53).jpg"
-                                    alt="Card image cap" />
-                                <div calssName="card-body">
-                                    <h4 calssName="card-title">Card title</h4>
-                                    <p calssName="card-text">Some quick example text to build on the card title and make up the bulk of the
-                card's content.</p>
-                                    <a calssName="btn btn-primary">Button</a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div calssName="col-md-4 clearfix d-none d-md-block">
-                            <div calssName="card mb-2">
-                                <img calssName="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/Food/4-col/img%20(45).jpg"
-                                    alt="Card image cap" />
-                                <div calssName="card-body">
-                                    <h4 calssName="card-title">Card title</h4>
-                                    <p calssName="card-text">Some quick example text to build on the card title and make up the bulk of the
-                card's content.</p>
-                                    <a calssName="btn btn-primary">Button</a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div calssName="col-md-4 clearfix d-none d-md-block">
-                            <div calssName="card mb-2">
-                                <img calssName="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/Food/4-col/img%20(51).jpg"
-                                    alt="Card image cap" />
-                                <div calssName="card-body">
-                                    <h4 calssName="card-title">Card title</h4>
-                                    <p calssName="card-text">Some quick example text to build on the card title and make up the bulk of the
-                card's content.</p>
-                                    <a calssName="btn btn-primary">Button</a>
-                                </div>
-                            </div>
-                        </div>
+                <div className="col-md-6 row test-der">
+                    <div className="col-md-6">
+                        <img className="img-testimonio" src="https://www.jovenesprogramadores.cl/wp/wp-content/uploads/2020/07/yerco.png" />
                     </div>
-
+                    <div className="col-md-6">
+                        <br />
+                        <h5>Marcos Perez</h5>
+                        <p className="perfil">Programmer</p>
+                        <p className="test-text">
+                            “Gracias por liberar y ofrecer este tipo de cursos en estos tiempos de crisis sanitaria. Es un excelente aporte para el auto aprendizaje guiado”.
+					</p>
+                    </div>
                 </div>
-
-
             </div>
+            <br /><br /><br />
+            <div className="main row">
+                <div className="col-md-12 test-izq testimonial-cuadro">
+                    <img className="img-testimonial" src="https://www.jovenesprogramadores.cl/wp/wp-content/uploads/2020/07/comillas.png" />
+                    <br /><br />
+                    <h5 className="titulo-testimonial">
+                        “Supe de ustedes por un amigo. Quiero actualizar y adquirir conocimientos en lenguajes que no conocía o que conocía pero por muchos años no practiqué y ya no recuerdo.
+                        Estoy feliz haciendo mi primer curso, lo encuentro muy bueno e interesante, recién terminé el primero. Recomiendo el programa recomiendo 100%”.
+					</h5>
+                    <br />
+                    <h3 className="h3-testimonial">Consuelo Contreras</h3>
+                </div>
+            </div>
+            <br /><br /><br />
+            <div className="row">
+                <div className="col-md-6">
 
-
+                </div>
+            </div>
         </div>
     );
 }
