d1861d91c5c892618640f1d2f62a670eb86ad0cb
Diego Emanuel Tolaba <diegotolaba09@gmail.com>
2020-10-17 19:19:45 -0300

    cambios generales en product card

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,10 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './ProductCard.css';
+import { useDispatch, useSelector } from 'react-redux';
+import * as action from '../redux/Action'
 
 export default function ProductCard(props) {
   const { name, image, price, description, id } = props;
+  const [ren, setRen] = useState(true)
+  const count = useSelector(store => store.count)
 
+	const dispatch = useDispatch()
+	useEffect(() => {
+
+	}, [ren, count])
+
+	if (!props) { return <h1>Loading...</h1> }
+
+	// const { name, image, price, description, id } = product
+
+	function counter() {
+		props.count = count
+  }
+
+  function handleAdd() {
+		// ren ? setRen(false) : setRen(true)
+
+		// let recoveredData = localStorage.getItem('prod')
+		// let search = JSON.parse(recoveredData)
+
+		// if (!recoveredData) {
+		// 	dispatch(action.countCart())
+		// 	let countCart = 1
+		// 	localStorage.setItem('count', countCart)
+		// 	return localStorage.setItem('prod', JSON.stringify([props]))
+
+		// }
+
+		// let fined = search.find(prod => prod.id == id)
+
+		// if (fined) {
+
+		// 	fined.count++
+		// 	let cleanData = search.filter((data) => data.id !== props.id)
+
+		// 	cleanData.push(fined)
+		// 	return localStorage.setItem('prod', JSON.stringify(cleanData))
+		// }
+		// let data = JSON.parse(recoveredData)
+		// let newProd = props
+		// data.push(newProd)
+		// dispatch(action.countCart())
+		// let countCart = data.length
+
+		// localStorage.setItem('prod', JSON.stringify(data))
+		// return localStorage.setItem('count', countCart)
+	}
   return (
     <div className="card">
       <Link className="titulo-link" to={`/products/${id}`}>
@@ -19,6 +69,7 @@ export default function ProductCard(props) {
           <p className="card-text">{`$ ${price}`}</p>
         </div>
       </Link>
+      <button type="button" class="btn btn-primary btn-m btn-cart-add" data-toggle="modal" data-target="#exampleModalCenter" data-backdrop="atencion" onClick={handleAdd} ><i class="fas fa-cart-plus"></i></button>
     </div>
   )
 }
