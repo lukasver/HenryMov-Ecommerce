2b45558a3b65661e3fb7dfbf22c1e85c8292b7b2
lukasver <l.verdiell@gmail.com>
2020-10-18 19:23:02 -0300

    modifico estilos en componente register y agrego boton en el navbar

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { useHistory } from "react-router-dom";
+import SingUp from '../img/cuentalogo.svg'
 import Logo from '../img/logoHenry.png'
 import SearchBar from './SearchBar.jsx';
 import * as action from '../redux/Action';
@@ -59,7 +60,7 @@ function Nav() {
             </div>
             <div className="col-md-3 utilidades">
                 <div className="main row">
-                    <div className="col-md-4">
+                    <div className="col-md-3">
                         <Link to='#' onClick={e => {e.preventDefault(); chatBot()}}>
                             <a className="linkIcons">
                                 <svg width="1.8em" height="1.8em" viewBox="0 0 16 16" class="bi bi-chat-dots" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
@@ -71,7 +72,7 @@ function Nav() {
                             </a>
                         </Link>
                     </div>
-                    <div className="col-md-4">
+                    <div className="col-md-3">
                         <Link to='/admin' className="linkIcons">
                             <svg width="1.8em" height="1.8em" viewBox="0 0 16 16" className="bi bi-person" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                 <path fillRule="evenodd" d="M10 5a2 2 0 1 1-4 0 2 2 0 0 1 4 0zM8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm6 5c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
@@ -80,7 +81,7 @@ function Nav() {
                             <span>Mi Cuenta</span>
                         </Link>
                     </div>
-                    <div className="col-md-4">
+                    <div className="col-md-3">
                         <Link to='/carrito' className="linkIcons">
                             <svg width="1.8em" height="1.8em" viewBox="0 0 16 16" className="bi bi-cart3" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                 <path fillRule="evenodd" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
@@ -88,6 +89,14 @@ function Nav() {
                             {countCart != 0 && countCart != null && <span className="cart-counter">{countCart}</span>}
                             <br />
                             <span>Carrito</span>
+                        </Link>
+                    </div>
+                        <div className="col-md-3">
+                        <Link to='/Register' className="linkIcons">
+                            {/*<i class="fas fa-user-plus"></i>*/}
+                            <img src={SingUp} className="bi" style={{heigth: "29px", width: "29px", filter: "invert(100%)"}} alt="Sing up"/>
+                            <br />
+                            <span>Registrarse</span>
                         </Link>
                     </div>
                 </div>
