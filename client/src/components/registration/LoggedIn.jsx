254e656a171693ea02e6654d701452925f774f8a
lukasver <l.verdiell@gmail.com>
2020-10-23 12:35:45 -0300

    redireccioando a home tras login

--- client/src/components/registration/LoggedIn.jsx
+++ client/src/components/registration/LoggedIn.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import axios from 'axios';
 import './LoggedIn.css';
 
 export default function LoggedIn() {
+
+  const handleLogin = (e) => {
+
+    e.preventDefault()
+    const email = document.getElementById("inputEmail").value
+    const password = document.getElementById("inputPassword").value
+
+    axios.post('http://localhost:3001/auth/login', {email, password}, {withCredentials: true})
+    .then(res => {
+      if (res.status === 200) {
+        console.log('si, es igual')
+        window.location="http://localhost:3000/"
+      }})
+    return
+    }
+
+
     return (
   <div class="container">
     <div class="row">
@@ -24,7 +42,7 @@ export default function LoggedIn() {
                 <input type="checkbox" class="custom-control-input" id="customCheck1"/>
                 <label class="custom-control-label" for="customCheck1" style={{"martin-top": "50px"}}>Recordar password</label>
               </div>
-              <button class="btn btn-lg btn-block text-uppercase botonlogin" type="submit">Sign in</button>
+              <button onClick={handleLogin} class="btn btn-lg btn-block text-uppercase botonlogin" type="submit">Sign in</button>
               <hr class="my-4"/>
               <button class="btn btn-lg btn-google btn-block text-uppercase" type="submit"><i class="fab fa-google mr-2"></i> Sign in with Google</button>
               <button class="btn btn-lg btn-facebook btn-block text-uppercase" type="submit"><i class="fab fa-facebook-f mr-2"></i> Sign in with Facebook</button>
