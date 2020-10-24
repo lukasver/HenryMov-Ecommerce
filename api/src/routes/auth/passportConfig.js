f83462deae8e10fbb62002519019f2aa8fa319f7
Diego Emanuel Tolaba <diegotolaba09@gmail.com>
2020-10-24 19:36:51 -0300

    cambios en login

--- api/src/routes/auth/passportConfig.js
+++ api/src/routes/auth/passportConfig.js
@@ -1,6 +1,9 @@
 const bcrypt = require('bcrypt');
 const { User } = require('../../db.js');
 const LocalStrategy = require('passport-local');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const GitHubStrategy = require('passport-github2').Strategy;
+const config = require('../config.js');
 
 module.exports = function (passport) {
     passport.use(
@@ -31,6 +34,76 @@ module.exports = function (passport) {
       // console.log('deserializacion', user)
       done(err, user)
     });
-  })
+  });
+
+//===============================================
+//     Estrategia de Google
+//===============================================
+    passport.use(new GoogleStrategy({
+      clientID: config.googleAuth.clientID,
+      clientSecret: config.googleAuth.clientSecret,
+      callbackURL: config.googleAuth.callbackURL,
+      passReqToCallback: true,
+      proxy: true
+      },
+      function(req, token, refreshToken, profile, done) {
+          process.nextTick(async function() {
+              console.log(profile);
+              const user = profile._json;
+              const password = 'hola1234';
+              let hashedPassword = await bcrypt.hash(password, 10);                  
+              User.findOrCreate({
+                  where: { email: user.email },
+                  defaults: {
+                      name: user.given_name,
+                      lastName: user.family_name,
+                      email: user.email,
+                      password: hashedPassword,
+                      otherAuth: 'yes'
+                  }
+              })
+              .then(res => res[0])
+              .then(user => {
+                  done(null, user);
+              })
+              .catch(err => done(err));
+          });
+      }            
+  ));
+
+//===============================================
+//     Estrategia de GitHub
+//===============================================
+  passport.use(new GitHubStrategy({
+      clientID: config.githubAuth.clientID,
+      clientSecret: config.githubAuth.clientSecret,
+      callbackURL: config.githubAuth.callbackURL,
+      passReqToCallback: true,
+      scope: ['user:email', 'read:user']
+      },
+      function(accessToken, refreshToken, params, profile, cb) {
+          process.nextTick(async function() {              
+              console.log(params);
+              const user = profile;
+              const password = 'hola1234';
+              let hashedPassword = await bcrypt.hash(password, 10);                  
+              User.findOrCreate({
+                  where: { email: user.emails[0].value },
+                  defaults: {
+                      name: user.displayName || user.username,
+                      lastName: user.username,
+                      email: user.emails[0].value,
+                      password: hashedPassword,
+                      otherAuth: 'yes'
+                  }
+              })
+              .then(res => res[0])
+              .then(user => {
+                  cb(null, user);
+              })
+              .catch(err => cb(err));            
+          });
+      }
+  ));
 }
 
---
+++ api/src/routes/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    'googleAuth' : {
+        'clientID': '401512495933-mdflotbp6e6ttt3mkosu5kb86c1ks6gs.apps.googleusercontent.com',
+        'clientSecret': 'u8wxMAA_2eCwfi3tk-gKyIkq',
+        'callbackURL': 'http://localhost:3001/auth/google/callback'
+    },
+    'githubAuth': {
+        'clientID': '43f7da3e3a7bdae69c35',
+        'clientSecret': '5bae70c05648141297ad253066f187b8ddbd7071',
+        'callbackURL': 'http://localhost:3001/auth/github/callback'
+    }
+};
